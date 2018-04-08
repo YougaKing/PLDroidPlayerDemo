405f58dafa8c0b1c304acaa0686696348c706d9d
YougaKing <yougakingwu@gmail.com>
2018-04-08 17:53:40 +0800

    Initial commit

---
+++ app/src/main/java/com/pili/pldroid/playerdemo/PLVideoViewActivity.java
@@ -0,0 +1,370 @@
+package com.pili.pldroid.playerdemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+import com.pili.pldroid.player.AVOptions;
+import com.pili.pldroid.player.PLOnAudioFrameListener;
+import com.pili.pldroid.player.PLOnBufferingUpdateListener;
+import com.pili.pldroid.player.PLOnCompletionListener;
+import com.pili.pldroid.player.PLOnErrorListener;
+import com.pili.pldroid.player.PLOnInfoListener;
+import com.pili.pldroid.player.PLOnPreparedListener;
+import com.pili.pldroid.player.PLOnVideoFrameListener;
+import com.pili.pldroid.player.PLOnVideoSizeChangedListener;
+import com.pili.pldroid.player.widget.PLVideoView;
+import com.pili.pldroid.playerdemo.utils.Config;
+import com.pili.pldroid.playerdemo.widget.MediaController;
+import com.pili.pldroid.playerdemo.widget.MediaController.OnClickSpeedAdjustListener;
+
+import java.io.InputStream;
+import java.util.Arrays;
+
+/**
+ * This is a demo activity of PLVideoView
+ */
+public class PLVideoViewActivity extends VideoPlayerBaseActivity {
+
+    private static final String TAG = PLVideoViewActivity.class.getSimpleName();
+
+    private PLVideoView mVideoView;
+    private Toast mToast = null;
+    private int mDisplayAspectRatio = PLVideoView.ASPECT_RATIO_FIT_PARENT;
+    private TextView mStatInfoTextView;
+    private MediaController mMediaController;
+
+    private boolean mIsLiveStreaming;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pl_video_view);
+
+        Logger.addLogAdapter(new AndroidLogAdapter() {
+            @Override
+            public boolean isLoggable(int priority, String tag) {
+                return BuildConfig.DEBUG;
+            }
+        });
+
+        String videoPath = getIntent().getStringExtra("videoPath");
+        mIsLiveStreaming = getIntent().getIntExtra("liveStreaming", 1) == 1;
+
+        mVideoView = findViewById(R.id.VideoView);
+
+        View loadingView = findViewById(R.id.LoadingView);
+        mVideoView.setBufferingIndicator(loadingView);
+
+        View mCoverView = findViewById(R.id.CoverView);
+        mVideoView.setCoverView(mCoverView);
+
+        mStatInfoTextView = findViewById(R.id.StatInfoTextView);
+
+        // 1 -> hw codec enable, 0 -> disable [recommended]
+        int codec = getIntent().getIntExtra("mediaCodec", AVOptions.MEDIA_CODEC_SW_DECODE);
+        AVOptions options = new AVOptions();
+        // the unit of timeout is ms
+        options.setInteger(AVOptions.KEY_PREPARE_TIMEOUT, 10 * 1000);
+        // 1 -> hw codec enable, 0 -> disable [recommended]
+        options.setInteger(AVOptions.KEY_MEDIACODEC, codec);
+        options.setInteger(AVOptions.KEY_LIVE_STREAMING, mIsLiveStreaming ? 1 : 0);
+        boolean disableLog = getIntent().getBooleanExtra("disable-log", false);
+//        options.setString(AVOptions.KEY_DNS_SERVER, "127.0.0.1");
+        options.setInteger(AVOptions.KEY_LOG_LEVEL, disableLog ? 5 : 0);
+        boolean cache = getIntent().getBooleanExtra("cache", false);
+        if (!mIsLiveStreaming && cache) {
+            options.setString(AVOptions.KEY_CACHE_DIR, Config.DEFAULT_CACHE_DIR);
+        }
+        boolean vcallback = getIntent().getBooleanExtra("video-data-callback", false);
+        if (vcallback) {
+            options.setInteger(AVOptions.KEY_VIDEO_DATA_CALLBACK, 1);
+        }
+        boolean acallback = getIntent().getBooleanExtra("audio-data-callback", false);
+        if (acallback) {
+            options.setInteger(AVOptions.KEY_AUDIO_DATA_CALLBACK, 1);
+        }
+        mVideoView.setAVOptions(options);
+
+        // Set some listeners
+        mVideoView.setOnPreparedListener(mPLOnPreparedListener);
+        mVideoView.setOnInfoListener(mOnInfoListener);
+        mVideoView.setOnVideoSizeChangedListener(mOnVideoSizeChangedListener);
+        mVideoView.setOnBufferingUpdateListener(mOnBufferingUpdateListener);
+        mVideoView.setOnCompletionListener(mOnCompletionListener);
+        mVideoView.setOnErrorListener(mOnErrorListener);
+        mVideoView.setOnVideoFrameListener(mOnVideoFrameListener);
+        mVideoView.setOnAudioFrameListener(mOnAudioFrameListener);
+
+        mVideoView.setVideoPath(videoPath);
+        mVideoView.setLooping(getIntent().getBooleanExtra("loop", false));
+
+        // You can also use a custom `MediaController` widget
+        mMediaController = new MediaController(this, !mIsLiveStreaming, mIsLiveStreaming);
+        mMediaController.setOnClickSpeedAdjustListener(mOnClickSpeedAdjustListener);
+        mVideoView.setMediaController(mMediaController);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        mVideoView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVideoView.pause();
+        mToast = null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mVideoView.stopPlayback();
+    }
+
+    public void onClickSwitchScreen(View v) {
+        mDisplayAspectRatio = (mDisplayAspectRatio + 1) % 5;
+        mVideoView.setDisplayAspectRatio(mDisplayAspectRatio);
+        switch (mVideoView.getDisplayAspectRatio()) {
+            case PLVideoView.ASPECT_RATIO_ORIGIN:
+                showToastTips("Origin mode");
+                break;
+            case PLVideoView.ASPECT_RATIO_FIT_PARENT:
+                showToastTips("Fit parent !");
+                break;
+            case PLVideoView.ASPECT_RATIO_PAVED_PARENT:
+                showToastTips("Paved parent !");
+                break;
+            case PLVideoView.ASPECT_RATIO_16_9:
+                showToastTips("16 : 9 !");
+                break;
+            case PLVideoView.ASPECT_RATIO_4_3:
+                showToastTips("4 : 3 !");
+                break;
+            default:
+                break;
+        }
+    }
+
+    /** 该对象用于监听播放器的 prepare 过程，该过程主要包括：创建资源、建立连接、请求码流等等，当 prepare 完成后，SDK 会回调该对象的 onPrepared 接口，下一步则可以调用播放器的 start() 启动播放 */
+    private PLOnPreparedListener mPLOnPreparedListener = new PLOnPreparedListener() {
+
+        @Override
+        public void onPrepared(int i) {
+            Logger.d("初始化完成");
+            mVideoView.start();
+        }
+    };
+
+    /** 该对象用于监听播放器的状态消息，在播放器启动后，SDK 会在播放器发生状态变化时调用该对象的 onInfo 方法，同步状态信息。 */
+    private PLOnInfoListener mOnInfoListener = new PLOnInfoListener() {
+        @Override
+        public void onInfo(int what, int extra) {
+//            Log.i(TAG, "OnInfo, what = " + what + ", extra = " + extra);
+            switch (what) {
+                case PLOnInfoListener.MEDIA_INFO_BUFFERING_START:
+                    Logger.d("开始缓冲");
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_BUFFERING_END:
+                    Logger.d("停止缓冲");
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_VIDEO_RENDERING_START:
+                    Logger.d("第一帧视频已成功渲染");
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_AUDIO_RENDERING_START:
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_VIDEO_FRAME_RENDERING:
+//                    Log.i(TAG, "video frame rendering, ts = " + extra);
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_AUDIO_FRAME_RENDERING:
+//                    Log.i(TAG, "audio frame rendering, ts = " + extra);
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_VIDEO_GOP_TIME:
+//                    Log.i(TAG, "Gop Time: " + extra);
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_SWITCHING_SW_DECODE:
+//                    Log.i(TAG, "Hardware decoding failure, switching software decoding!");
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_METADATA:
+                    Logger.d("读取到 metadata 信息\n" + mVideoView.getMetadata().toString());
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_VIDEO_BITRATE:
+                case PLOnInfoListener.MEDIA_INFO_VIDEO_FPS:
+                    updateStatInfo();
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_CONNECTED:
+                    Logger.d("连接成功");
+                    break;
+                case PLOnInfoListener.MEDIA_INFO_VIDEO_ROTATION_CHANGED:
+//                    Log.i(TAG, "Rotation changed: " + extra);
+                default:
+                    break;
+            }
+        }
+    };
+
+    private PLOnErrorListener mOnErrorListener = new PLOnErrorListener() {
+        @Override
+        public boolean onError(int errorCode) {
+            switch (errorCode) {
+                case PLOnErrorListener.ERROR_CODE_IO_ERROR:
+                    return false;
+                case PLOnErrorListener.ERROR_CODE_OPEN_FAILED:
+                    break;
+                case PLOnErrorListener.ERROR_CODE_SEEK_FAILED:
+                    showToastTips("failed to seek !");
+                    break;
+                default:
+                    showToastTips("unknown error !");
+                    break;
+            }
+            finish();
+            return true;
+        }
+    };
+
+    /**
+     * 该对象用于监听播放结束的消息，关于该回调的时机，有如下定义：
+     * 如果是播放文件，则是播放到文件结束后产生回调
+     * 如果是在线视频，则会在读取到码流的EOF信息后产生回调，回调前会先播放完已缓冲的数据
+     * 如果播放过程中产生onError，并且没有处理的话，最后也会回调本接口
+     * 如果播放前设置了 setLooping(true)，则播放结束后会自动重新开始，不会回调本接口
+     * 如果同时将 AVOptions.KEY_FAST_OPEN 与 AVOptions.KEY_SEEK_MODE 设置为 1，并且希望在收到本接口后播放同一个视频，需要在 start 后手动调用 seekTo(0)
+     */
+    private PLOnCompletionListener mOnCompletionListener = new PLOnCompletionListener() {
+        @Override
+        public void onCompletion() {
+            showToastTips("Play Completed !");
+            if (!mIsLiveStreaming) {
+                mMediaController.refreshProgress();
+            }
+            //finish();
+        }
+    };
+
+    /** 该回调用于监听当前播放器已经缓冲的数据量占整个视频时长的百分比，在播放直播流中无效，仅在播放文件和回放时才有效 */
+    private PLOnBufferingUpdateListener mOnBufferingUpdateListener = new PLOnBufferingUpdateListener() {
+        @Override
+        public void onBufferingUpdate(int precent) {
+            Logger.d("onBufferingUpdate: " + precent);
+        }
+    };
+
+    private PLOnVideoSizeChangedListener mOnVideoSizeChangedListener = new PLOnVideoSizeChangedListener() {
+        @Override
+        public void onVideoSizeChanged(int width, int height) {
+//            Log.i(TAG, "onVideoSizeChanged: width = " + width + ", height = " + height);
+        }
+    };
+
+    private PLOnVideoFrameListener mOnVideoFrameListener = new PLOnVideoFrameListener() {
+        @Override
+        public void onVideoFrameAvailable(byte[] data, int size, int width, int height, int format, long ts) {
+//            Log.i(TAG, "onVideoFrameAvailable: " + size + ", " + width + " x " + height + ", " + format + ", " + ts);
+            if (format == PLOnVideoFrameListener.VIDEO_FORMAT_SEI && bytesToHex(Arrays.copyOfRange(data, 19, 23)).equals("ts64")) {
+                // If the RTMP stream is from Qiniu
+                // Add &addtssei=true to the end of URL to enable SEI timestamp.
+                // Format of the byte array:
+                // 0:       SEI TYPE                    This is part of h.264 standard.
+                // 1:       unregistered user data      This is part of h.264 standard.
+                // 2:       payload length              This is part of h.264 standard.
+                // 3-18:    uuid                        This is part of h.264 standard.
+                // 19-22:   ts64                        Magic string to mark this stream is from Qiniu
+                // 23-30:   timestamp                   The timestamp
+                // 31:      0x80                        Magic hex in ffmpeg
+//                Log.i(TAG, " timestamp: " + Long.valueOf(bytesToHex(Arrays.copyOfRange(data, 23, 31)), 16));
+            }
+        }
+    };
+
+    private PLOnAudioFrameListener mOnAudioFrameListener = new PLOnAudioFrameListener() {
+        @Override
+        public void onAudioFrameAvailable(byte[] data, int size, int samplerate, int channels, int datawidth, long ts) {
+//            Log.i(TAG, "onAudioFrameAvailable: " + size + ", " + samplerate + ", " + channels + ", " + datawidth + ", " + ts);
+        }
+    };
+
+    private OnClickSpeedAdjustListener mOnClickSpeedAdjustListener = new OnClickSpeedAdjustListener() {
+        @Override
+        public void onClickNormal() {
+            // 0x0001/0x0001 = 2
+            mVideoView.setPlaySpeed(0X00010001);
+        }
+
+        @Override
+        public void onClickFaster() {
+            // 0x0002/0x0001 = 2
+            mVideoView.setPlaySpeed(0X00020001);
+        }
+
+        @Override
+        public void onClickSlower() {
+            // 0x0001/0x0002 = 0.5
+            mVideoView.setPlaySpeed(0X00010002);
+        }
+    };
+
+    private String bytesToHex(byte[] bytes) {
+        char[] hexArray = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    private void showToastTips(final String tips) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                mToast = Toast.makeText(PLVideoViewActivity.this, tips, Toast.LENGTH_SHORT);
+                mToast.show();
+            }
+        });
+    }
+
+    private void updateStatInfo() {
+        long bitrate = mVideoView.getVideoBitrate() / 1024;
+        final String stat = bitrate + "kbps, " + mVideoView.getVideoFps() + "fps";
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mStatInfoTextView.setText(stat);
+            }
+        });
+    }
+
+    private void printLog() {
+        try {
+            Process p = Runtime.getRuntime().exec("logcat");
+            final InputStream is = p.getInputStream();
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        int len = 0;
+                        byte[] buf = new byte[1024];
+                        while (-1 != (len = is.read(buf))) {
+
+                        }
+                    } catch (Exception e) {
+                        Log.d("writelog", "read logcat process failed. message: " + e.getMessage());
+                    }
+                }
+            }.start();
+        } catch (Exception e) {
+            Log.d("writelog", "open logcat process failed. message: " + e.getMessage());
+        }
+    }
+}
